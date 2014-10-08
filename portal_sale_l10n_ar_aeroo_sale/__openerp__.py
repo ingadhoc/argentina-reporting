322574dd0e5b1d014f6e459511d55f91e321d8d7
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-10-08 20:37:55 -0300

    IMP aeroo reports

---
+++ portal_sale_l10n_ar_aeroo_sale/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Argentinian Like Sale Order Integration With Portal Sale',
+    'version': '1.0',
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+Argentinian Like Sale Order Integration With Portal Sale
+========================================================
+    """,
+    'author':  'Ingenieria ADHOC',
+    'website': 'www.ingadhoc.com',
+    'images': [
+    ],
+    'depends': [
+        'l10n_ar_aeroo_sale',
+        'l10n_ar_aeroo_invoice',
+        'portal_sale',
+    ],
+    'data': [
+        'sale_order_template.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
