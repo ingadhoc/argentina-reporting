a4f207f1a341dc8d3a1b253ee73b8dca45efab66
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-09-07 15:52:17 -0300

    [FIX] migration scripts

--- l10n_ar_aeroo_payment_group/__manifest__.py
+++ l10n_ar_aeroo_payment_group/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Argentinian Like Payment Aeroo Report',
-    'version': '11.0.1.1.0',
+    'version': '11.0.1.2.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
--- l10n_ar_aeroo_purchase/migrations/11.0.1.2.0/post-migration.py
+++ l10n_ar_aeroo_purchase/migrations/11.0.1.2.0/post-migration.py
@@ -4,4 +4,4 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(
-        env.cr, 'l10n_ar_aeroo_invoice', 'migrations/11.0.1.1.0/mig_data.xml')
+        env.cr, 'l10n_ar_aeroo_purchase', 'migrations/11.0.1.2.0/mig_data.xml')
