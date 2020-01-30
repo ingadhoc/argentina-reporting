c936e71ddec8c438f8ebb8d3a10ef363bb78bae7
Lucas L. Soto <ls@adhoc.com.ar>
2020-01-30 13:48:06 -0300

    [MIG] Bump all repo's modules to version 13.0.

--- l10n_ar_aeroo_base/__manifest__.py
+++ l10n_ar_aeroo_base/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Base Module For Argentinian Like Aeroo Reports',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
--- l10n_ar_aeroo_purchase/__manifest__.py
+++ l10n_ar_aeroo_purchase/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Argentinian Like Purchase Order Aeroo Report',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
