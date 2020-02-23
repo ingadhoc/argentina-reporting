3851b8f3238f2f2b0735530ccdbd9745746c6625
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-02-23 09:21:15 -0300

    [MIG] l10n_ar_aeroo_invoice: Migration to 13.0

--- l10n_ar_aeroo_invoice/__manifest__.py
+++ l10n_ar_aeroo_invoice/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Argentinian Like Invoice Aeroo Report',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
@@ -39,7 +39,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
