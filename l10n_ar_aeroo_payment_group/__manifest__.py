e1d448a3169ed04d264e2056bc089a51898a2db6
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-02-25 17:08:52 -0300

    [MIG] l10n_ar_aeroo_payment_group: Migration to 13.0

--- l10n_ar_aeroo_payment_group/__manifest__.py
+++ l10n_ar_aeroo_payment_group/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Argentinian Like Payment Aeroo Report',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
@@ -42,7 +42,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
